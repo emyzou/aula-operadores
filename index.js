afd433a95dbbb02ee397f0e57731c7b2c9ea8fa2
Emilly Agatha <135267138+emyzou@users.noreply.github.com>
2023-08-24 16:41:11 -0300

    index.js

---
+++ index.js
@@ -0,0 +1,56 @@
+// this is a js file
+//Emilly Agatha
+
+//exercicio 1.
+/* a.false
+   b.false
+   c.true
+   d.boolean
+*/
+
+//exercicio 2. e 3.
+/* R: Será impresso os dois números repetidos,não somados.
+      Está faltando o código Number na frente das variaveis na
+      const soma e o ().
+*/
+
+//B.1 Exercícios de escrita de código
+/*
+let idade = prompt("Qual a sua idade?");
+let idadeAmigo = prompt("Qual a idade do seu melhor amigo(a)?");
+
+console.log("Sua idade é maior do que a do seu melhor amigo?", idade > idadeAmigo);
+console.log("A diferença de idade dos amigos é:", Number (idade) - Number (idadeAmigo));
+*/
+
+//2. 
+/*
+let par = prompt("Insira um número par.");
+console.log(par % 2);
+c) notei quem todos os números pares resultavam em 0
+   e os ímpares restavam 1.
+*/
+
+//3. 
+/*
+let idade = prompt("Qual a sua idade?")
+let meses = 12;
+let dias = 365;
+let horas = 24;
+
+console.log(meses * Number (idade));
+console.log(dias * Number (idade));
+console.log(horas * Number (idade));
+*/
+
+//4.
+/*
+let number1 = prompt("Digite um número.");
+let number2 = prompt("digite outro número.");
+
+console.log("O primeiro número é maior que o segundo?", Number (number1) > Number(number2));
+console.log("O primeiro número é igual ao segundo?", Number (number1) === Number (number2));
+console.log("O primeiro número é divisivel pelo segundo?", Boolean (Number (number1) / Number (number2)));
+console.log("O segundo número é divisivel pelo primeiro?", Boolean (Number (number1) / Number (number2))); 
+*/
+
